f6372440c075a3d9df06b6981ef763a5f4261f86
Ivan Buccella <ivan@buccella.me>
2023-09-30 02:00:25 +0200

    Updated run.sh

--- run.sh
+++ run.sh
@@ -17,8 +17,8 @@ if [ ! -d "/Applications/MuseScore 3.app" ]; then
     echo "MuseScore 3 is not installed"
     exit 1
 fi
-
-docker-compose up -d --build
+docker-compose build
+docker-compose up -d
 if [ ! $? -eq 0 ]; then
     echo "The docker compose command has failed"
     exit 1
